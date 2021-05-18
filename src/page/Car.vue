0175f660439bf625b0d4155e5490b2725b5496dc
sirodeneko <2487252242@qq.com>
2021-05-18 20:23:03 +0800

    不写了

--- src/page/Car.vue
+++ src/page/Car.vue
@@ -1,7 +1,17 @@
 <template>
   <div id="car" class="center">
-      好快的的车车
-      
+    <div class="car-content">
+      <el-row :gutter="20">
+        <el-col :span="12" class="car-content-left">
+          <img :src="car.url" alt="" />
+        </el-col>
+        <el-col :span="12" class="car-content-right">
+          <div class="car-title">{{ car.name }}</div>
+          <div class="car-"></div>
+        </el-col>
+
+      </el-row>
+    </div>
   </div>
 </template>
 
@@ -10,12 +20,12 @@ export default {
   name: "Car",
   data() {
     return {
-        car:this.$store.getters.getCar,
+      car: this.$store.getters.getCar,
     };
   },
   methods: {
     load() {
-        console.log(this.car);
+      console.log(this.car);
     },
   },
   created() {
@@ -24,4 +34,24 @@ export default {
 };
 </script>
 
-<style lang="less"></style>
+<style lang="less">
+#car {
+  .car-content {
+    margin-top: 10px;
+    .car-content-left {
+      background-color: rosybrown;
+    }
+    .car-content-right {
+      background-color: rosybrown;
+      text-align: start;
+      .car-title {
+        font-size: 26px;
+        line-height: 1;
+        font-weight: bold;
+        color: #111e36;
+        padding: 5px 0;
+      }
+    }
+  }
+}
+</style>
