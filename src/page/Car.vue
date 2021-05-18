98b03a224e6628112477722cc7faeb1dc4150f46
sirodeneko <2487252242@qq.com>
2021-05-18 16:57:39 +0800

    car

---
+++ src/page/Car.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="index" class="center"></div>
+</template>
+
+<script>
+export default {
+  name: "Car",
+  data() {
+    return {};
+  },
+  methods: {
+    load() {},
+  },
+  created() {
+    this.load();
+  },
+};
+</script>
+
+<style lang="less"></style>
